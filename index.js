5cb9b58c2d724818aa767bccd51a722c409b7fce
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-07-25 00:23:37 +0500

    Resolve some issues with output conversion

--- index.js
+++ index.js
@@ -30,15 +30,11 @@ function operate(a, b, operator) {
     return operations[operator](a, b);
 }
 
-// THE MAIN CONDITIONS
-
 let operatorIsClicked = false;
 let firstNumber = null;
 let secondNumber = null;
 let tempOperator = null;
 
-// 12 digits maximum
-// 
 const numbers = document.querySelector('.numbers');
 const operators = document.querySelector('.operators');
 const dot = document.querySelector('.dot');
@@ -49,7 +45,7 @@ const outputFiled = document.querySelector('.output-content');
 
 const clear = document.querySelector('.cancel');
 
-numbers.addEventListener('click', function(e) {
+numbers.addEventListener('click', function (e) {
     if (e.target.classList.contains('number') && outputFiled.textContent.length <= 12) {
         if (outputFiled.textContent == '0') outputFiled.textContent = '';
 
@@ -60,7 +56,8 @@ numbers.addEventListener('click', function(e) {
         }
 
         outputFiled.textContent += e.target.textContent;
-        if (outputFiled.textContent.length > 12) outputFiled.textContent = outputFiled.textContent.slice(0, 12);
+        if (outputFiled.textContent.length > 12)
+            outputFiled.textContent = String(Number(outputFiled.textContent).toExponential(2));
     }
 });
 
@@ -73,7 +70,7 @@ clear.addEventListener('click', () => {
 dot.addEventListener('click', () => {
     outputFiled.textContent.indexOf('.') != -1 || (outputFiled.textContent += '.')
 });
-reverse.addEventListener('click', function() {
+reverse.addEventListener('click', function () {
     const condition = outputFiled.textContent.indexOf('-');
     if (Math.round(Number(outputFiled.textContent)) != 0 && condition == -1) {
         outputFiled.textContent = '-' + outputFiled.textContent;
@@ -81,14 +78,15 @@ reverse.addEventListener('click', function() {
         outputFiled.textContent = outputFiled.textContent.replace('-', '');
     }
 });
-percentButton.addEventListener('click', function() {
+percentButton.addEventListener('click', function () {
     outputFiled.textContent = `${Number(outputFiled.textContent) / 100}`
-    if (outputFiled.textContent.length > 12) outputFiled.textContent = outputFiled.textContent.slice(0, 12);
+    if (outputFiled.textContent.length > 12)
+        outputFiled.textContent = String(Number(outputFiled.textContent).toExponential(2));
 });
 
 // Operators
 
-operators.addEventListener('click', function(e) {
+operators.addEventListener('click', function (e) {
     operatorIsClicked = true;
 
     if (tempOperator != null) {
@@ -96,12 +94,13 @@ operators.addEventListener('click', function(e) {
         firstNumber = operate(firstNumber, secondNumber, tempOperator);
         outputFiled.textContent = `${firstNumber}`;
 
-        if (outputFiled.textContent.length > 12) outputFiled.textContent = outputFiled.textContent.slice(0, 12);
+        if (outputFiled.textContent.length > 12)
+            outputFiled.textContent = String(Number(outputFiled.textContent).toExponential(2));
 
         secondNumber = null;
         tempOperator = null;
     }
-    
+
     if (tempOperator == null) tempOperator = e.target.textContent;
     if (tempOperator == '=') tempOperator = null;
 })
